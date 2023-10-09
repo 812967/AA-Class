5efdf5055cd1f27c430f6ea36e4f761c97f0d7d7
Calvin <812967@seq.org>
2023-10-08 19:51:24 -0700

    Squares work

--- 6 - Particle Systems/Particle.js
+++ 6 - Particle Systems/Particle.js
@@ -3,7 +3,7 @@ function Particle(loc) {
     this.vel = new JSVector(0, 5);
     this.angle = Math.random() * ((5*Math.PI/6) - (Math.PI/6)) + (Math.PI/6);
     this.vel.setDirection(this.angle*-1);
-    this.shape = Math.floor(Math.random());
+    this.shape = Math.floor(Math.random() * 2);
     this.color = Math.floor(Math.random() * 7);
 }
 
@@ -71,43 +71,43 @@ Particle.prototype.render = function () {//render the shapes
         if (this.color === 0) {
             context.fillStyle = "rgba(255, 19, 0, 1)";
             context.beginPath();
-            context.rect(200, 200, 10, 10);
+            context.rect(this.loc.x, this.loc.y, 10, 10);
             context.fill();
         }
         else if (this.color === 1) {
             context.fillStyle = "rgba(255, 142, 0, 1)";
             context.beginPath();
-            context.rect(200, 200, 10, 10);
+            context.rect(this.loc.x, this.loc.y, 10, 10);
             context.fill();
         }
         else if (this.color === 2) {
             context.fillStyle = "rgba(255, 219, 0, 1)";
             context.beginPath();
-            context.rect(200, 200, 10, 10);
+            context.rect(this.loc.x, this.loc.y, 10, 10);
             context.fill();
         }
         else if (this.color === 3) {
             context.fillStyle = "rgba(0, 255, 44, 1)";
             context.beginPath();
-            context.rect(200, 200, 10, 10);
+            context.rect(this.loc.x, this.loc.y, 10, 10);
             context.fill();
         }
         else if (this.color === 4) {
             context.fillStyle = "rgba(50, 148, 243, 1)";
             context.beginPath();
-            context.rect(200, 200, 10, 10);
+            context.rect(this.loc.x, this.loc.y, 10, 10);
             context.fill();
         }
         else if (this.color === 5) {
             context.fillStyle = "rgba(115, 62, 255, 1)";
             context.beginPath();
-            context.rect(200, 200, 10, 10);
+            context.rect(this.loc.x, this.loc.y, 10, 10);
             context.fill();
         }
         else if (this.color === 6) {
             context.fillStyle = "rgba(248, 39, 255, 1)";
             context.beginPath();
-            context.rect(200, 200, 10, 10);
+            context.rect(this.loc.x, this.loc.y, 10, 10);
             context.fill();
         }
     }
