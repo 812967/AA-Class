10bfdaca4df92dd8c8fc63a8bf82f9dcb25eb72a
Calvin <812967@seq.org>
2023-10-19 17:13:50 -0700

    Ship planet working

--- 8 - Flying Triangle With A Tail/planet.js
+++ 8 - Flying Triangle With A Tail/planet.js
@@ -1,20 +1,50 @@
 function Planet(r){
-    this.loc = new JSVector(Math.random()*canvas.width, Math.random()*canvas.height);
+    this.loc = new JSVector(Math.random()*(canvas.width-200+1)-100, Math.random()*(canvas.height-200+1)-100);
     this.vel = new JSVector(0,0);
     this.radius = r;
 }
 
 Planet.prototype.run = function (){
     this.update();
+    this.checkEdges();
     this.render();
 }
 
 Planet.prototype.update = function (){//move away from ship if ship gets close 
-    if(this.loc.distanceSquared(ship.loc)<200){
-        this.vel = ship.vel;
-    }else{this.vel.setMagnitude(0);}
+    if(this.loc.distance(ship.loc)<200){
+        this.vel = ship.vel.copy();
+        this.vel.setMagnitude(this.vel.getMagnitude()*0.75);
+    }
+    if(this.loc.distance(ship.loc)<50){
+        this.loc.x = Math.random()*canvas.width;
+        this.loc.y = Math.random()*canvas.height;
+        this.vel = new JSVector(0,0);
+    }
     this.loc = JSVector.addGetNew(this.loc, this.vel);
 }
+
+Planet.prototype.checkEdges = function () {
+    if (this.loc.x > canvas.width) {
+        this.loc.x = Math.random()*canvas.width;
+        this.loc.y = Math.random()*canvas.height;
+        this.vel = new JSVector(0,0);
+    }
+    if (this.loc.x < 0) {
+        this.loc.x = Math.random()*canvas.width;
+        this.loc.y = Math.random()*canvas.height;
+        this.vel = new JSVector(0,0);
+    }
+    if (this.loc.y > canvas.height) {
+        this.loc.x = Math.random()*canvas.width;
+        this.loc.y = Math.random()*canvas.height;
+        this.vel = new JSVector(0,0);
+    }
+    if (this.loc.y < 0) {
+        this.loc.x = Math.random()*canvas.width;
+        this.loc.y = Math.random()*canvas.height;
+        this.vel = new JSVector(0,0);
+    }
+  }
 
 Planet.prototype.render = function (){
     context.fillStyle = "rgba(38, 91, 106, 1)";
--- 8 - Flying Triangle With A Tail/ship.js
+++ 8 - Flying Triangle With A Tail/ship.js
@@ -6,22 +6,40 @@ function Ship(x, y){
 
 Ship.prototype.run = function (){
     this.update();
+    this.checkEdges();
     this.render();
 }
 
 Ship.prototype.update = function (){
     this.acc = JSVector.subGetNew(planet.loc, this.loc);
     this.acc.normalize();
-    this.acc.multiply(0.05);
+    this.acc.multiply(0.03);
     this.vel = JSVector.addGetNew(this.vel, this.acc);
-    this.vel.limit(3);
+    this.vel.limit(2);
     this.loc = JSVector.addGetNew(this.loc, this.vel);
 }
 
+Ship.prototype.checkEdges = function () {
+    if (this.loc.x > canvas.width) {
+        this.loc.x = 0;
+    }
+    if (this.loc.x < 0) {
+        this.loc.x = canvas.width;
+    }
+    if (this.loc.y > canvas.height) {
+      this.loc.y = 0;
+    }
+    if (this.loc.y < 0) {
+      this.loc.y = canvas.height;
+    }
+  }
+
 Ship.prototype.render = function (){
+    let rotateAngle = this.vel.getDirection();
     context.save();
     //Ship
     context.translate(this.loc.x, this.loc.y);
+    context.rotate(rotateAngle);
     context.beginPath();
     context.moveTo(20, 0);
     context.lineTo(-10, 10);
@@ -33,19 +51,19 @@ Ship.prototype.render = function (){
     context.fill();
     context.stroke();
     //Flame
-    let fl = this.vel.getMagnitude()*5;
+    let fl = this.vel.getMagnitude()*30;
     let opacity = Math.random();
     context.beginPath();
     context.moveTo(-10, 0);
     context.lineTo(-15, 5);
-    context.lineTo(-60-fl, 0);
+    context.lineTo(-10-fl, 0);
     context.closePath();
     context.fillStyle = "rgba(170, 114, 57, " + opacity + ")";
     context.fill();
     context.beginPath();
     context.moveTo(-10, 0);
     context.lineTo(-15, -5);
-    context.lineTo(-60-fl, 0);
+    context.lineTo(-10-fl, 0);
     context.closePath();
     context.fillStyle = "rgba(170, 114, 57, " + opacity + ")";
     context.fill();
