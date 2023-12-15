01717ff8eed7595c7e0bd69219232945474cc2db
Calvin <812967@seq.org>
2023-12-14 23:29:23 -0800

    Food shape changed

--- 7 - Ecosystem (With Particle Systems)/Particle.js
+++ 7 - Ecosystem (With Particle Systems)/Particle.js
@@ -7,9 +7,17 @@ function Particle(loc) {
 }
 
 Particle.prototype.render = function () {//green rectangle with round edges 
+    context.save();
+    context.translate(this.loc.x, this.loc.y);
     context.fillStyle = "rgba(12, 131, 48, " + this.opacity + ")";
     context.beginPath();
-    context.roundRect(this.loc.x, this.loc.y, 25, 25, 5);
+    context.moveTo(0, 20);
+    context.lineTo(10, 0);
+    context.lineTo(-10, 0);
+    context.moveTo(0, -20);
+    context.lineTo(10, 0);
+    context.lineTo(-10, 0);
     context.fill();
+    context.restore();
 
 }
